0c39f36631390d80b2b7971c52fb98087a776722
John Perez <55162758+jjperez9000@users.noreply.github.com>
2023-09-22 18:54:30 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HexagonalChessBoard from "./components/board.jsx";
+function App() {
+  return (
+    <div classname="App">
+      <HexagonalChessBoard/>
+    </div>
+  );
+}
+
+export default App;
